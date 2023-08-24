b731849663cf074884555f4ccf6a62cf30da9640
Christopher Variara <cvariara@gmail.com>
2023-08-23 20:05:40 -0400

    Removed prompts for players & added buttons for player selections

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 const choices = ["Rock", "Paper", "Scissors"];
 
+let playerScore = 0;
+let computerScore = 0;
+
+const moves = document.querySelectorAll('.move');
+moves.forEach((move) => {
+  move.addEventListener('click', () => {
+    if (move.classList.contains('rock')) {
+      console.log(playRound("Rock", getComputerChoice()));
+    } else if (move.classList.contains('paper')) {
+      console.log(playRound("Paper", getComputerChoice()));
+    } else {
+      console.log(playRound("Scissors", getComputerChoice()));
+    }
+  })
+})
+
+// const playerRock = document.querySelector('.rock');
+// const playerPaper = document.querySelector('.paper');
+// const playerScissors = document.querySelector('.scissors');
+
 function getComputerChoice() {
   const random = Math.floor(Math.random() * choices.length);
   return choices[random];
 }
 
 function playRound(playerSelection, computerSelection) {
-  playerSelection =
-    playerSelection.charAt(0).toUpperCase() +
-    playerSelection.slice(1).toLowerCase();
-
   switch (playerSelection) {
     case "Rock":
       if (computerSelection === "Paper") {
         return "You Lose! Paper beats Rock";
+
       } else if (computerSelection === "Scissors") {
         return "You Win! Rock beats Scissors";
       }
@@ -38,17 +55,15 @@ function playRound(playerSelection, computerSelection) {
   return "It's a tie!";
 }
 
-let playerScore = 0;
-let computerScore = 0;
-
 function game() {
-  for (let i = 1; i <= 5; i++) {
-    console.log("Game: " + i);
-    const playerSelection = prompt("What do you choose?: ");
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-    getScore(playRound(playerSelection, computerSelection));
-  }
+  // for (let i = 1; i <= 5; i++) {
+  //   console.log("Game: " + i);
+  //   const playerSelection = prompt("What do you choose?: ");
+  //   const computerSelection = getComputerChoice();
+  //   console.log(playRound(playerSelection, computerSelection));
+  //   getScore(playRound(playerSelection, computerSelection));
+  // }
+
   if (playerScore > computerScore) {
     alert(
       "Player wins!\nPlayer Score: " +
@@ -84,4 +99,4 @@ function getScore(message) {
   }
 }
 
-game();
+// game();
